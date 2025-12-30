3793dec9dc6ac746714e2cfdedd101f7f48f1c12
chandru12588 <chandru.jerry@gmail.com>
2025-12-30 19:45:32 +0530

    updated indexjs

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/mail", mailRoutes);
 
 // --- Start Server ---
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () =>
   console.log("🚀 Backend running on port", PORT)
 );
